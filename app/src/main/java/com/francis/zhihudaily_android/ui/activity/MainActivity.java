134ffa6984e20671728fb3a308abe6f7fa1a81f4
geniusvjr <geniusvjr@gmail.com>
2016-08-30 18:22:28 +0800

    更新部分知乎日报 API

--- app/src/main/java/com/francis/zhihudaily_android/ui/activity/MainActivity.java
+++ app/src/main/java/com/francis/zhihudaily_android/ui/activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.francis.zhihudaily_android.ui.activity;
 
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.BaseAdapter;
 import com.francis.zhihudaily_android.R;
 import com.francis.zhihudaily_android.constant.Constants;
@@ -12,16 +17,37 @@ import com.francis.zhihudaily_android.ui.activity.base.BaseActivity;
 public class MainActivity extends BaseActivity {
 
 
+	private DrawerLayout mDrawerLayout;
+	private Toolbar mToolbar;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		layoutId = R.layout.activity_main;
 		super.onCreate(savedInstanceState);
 
-		TabLayout tabLayout = (TabLayout) findViewById(R.id.main_pager_tabs);
-		ViewPager viewPager = (ViewPager) findViewById(R.id.main_pager);
+		mToolbar = (Toolbar) findViewById(R.id.layout_toolbar);
+		setSupportActionBar(mToolbar);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+		actionBar.setDisplayHomeAsUpEnabled(true);
 
-		viewPager.setOffscreenPageLimit(Constants.PAGE_COUNT);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.layout_drawer);
+		mToolbar = (Toolbar) findViewById(R.id.layout_toolbar);
+		mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+
+		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+		if(navigationView != null){
+			navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+				@Override
+				public boolean onNavigationItemSelected(MenuItem item) {
+					return false;
+				}
+			});
+		}
+
 
 
 	}
---
+++ app/src/main/java/com/francis/zhihudaily_android/ui/fragment/MainFragment.java
@@ -0,0 +1,9 @@
+package com.francis.zhihudaily_android.ui.fragment;
+
+/**
+ * @author taoc @ Zhihu Inc.
+ * @since 08-30-2016
+ *
+ * 首页
+ */
+public class MainFragment {}
