acfad943abd7afc4944bdc241ad52b2b7240f101
geniusvjr <geniusvjr@gmail.com>
2016-08-30 21:38:20 +0800

    添加轮播条的 dependencies

--- app/src/main/java/com/francis/zhihudaily_android/ui/fragment/MainFragment.java
+++ app/src/main/java/com/francis/zhihudaily_android/ui/fragment/MainFragment.java
@@ -1,9 +1,28 @@
 package com.francis.zhihudaily_android.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * @author taoc @ Zhihu Inc.
  * @since 08-30-2016
  *
  * 首页
  */
-public class MainFragment {}
+public class MainFragment extends Fragment{
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
