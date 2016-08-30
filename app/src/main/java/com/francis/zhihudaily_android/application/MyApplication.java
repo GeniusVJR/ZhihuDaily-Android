4ee785a68663faebdee3f409e8e076875a52185b
geniusvjr <geniusvjr@gmail.com>
2016-08-30 15:17:23 +0800

    修改 application 类

--- app/src/main/java/com/francis/zhihudaily_android/application/MyApplication.java
+++ app/src/main/java/com/francis/zhihudaily_android/application/MyApplication.java
@@ -1,11 +1,6 @@
 package com.francis.zhihudaily_android.application;
 
 import android.app.Application;
-import android.content.Context;
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 /**
  * @author taoc @ Zhihu Inc.
@@ -19,18 +14,5 @@ public class MyApplication extends Application{
 	public void onCreate() {
 		super.onCreate();
 		sMyApplication = this;
-
-		initImageLoader(getApplicationContext());
-	}
-
-	//初始化 ImageLoader
-	public static void initImageLoader(Context context){
-		ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(context)
-				.denyCacheImageMultipleSizesInMemory()
-				.threadPriority(Thread.NORM_PRIORITY - 2)
-				.diskCacheFileNameGenerator(new Md5FileNameGenerator())
-				.tasksProcessingOrder(QueueProcessingType.LIFO)
-				.build();
-		ImageLoader.getInstance().init(configuration);
 	}
 }
