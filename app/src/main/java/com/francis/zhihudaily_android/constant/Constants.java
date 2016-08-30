a4131172267514b4462a5d22c9010a21352fd881
geniusvjr <geniusvjr@gmail.com>
2016-08-30 16:46:35 +0800

    初始化 NavigationView

--- app/src/main/java/com/francis/zhihudaily_android/constant/Constants.java
+++ app/src/main/java/com/francis/zhihudaily_android/constant/Constants.java
@@ -11,7 +11,5 @@ public class Constants {
 
 	public static final class Strings {
 		public static final String ZHIHU_QUESTION_LINK_PREFIX = "http://www.zhihu.com/question/";
-		public static final String SHARE_FROM_ZHIHU = " 分享自知乎网";
-		public static final String MULTIPLE_DISCUSSION = "这里包含多个知乎讨论，请点击后选择";
 	}
 }
